8786258b0a456f55b572e9c2ed1deaada554153a
Cleyton Polidoro <Cleyton.polidoro@gmail.com>
2024-10-09 19:45:16 -0300

    Exception handling - delete

---
+++ src/main/java/com/example/course/resources/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.example.course.resources.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
